7e3f004c367b90f68516ddbb288e1986962ecf7e
Soroush Rasti <rasti.soroush@gmail.com>
2020-08-17 10:50:12 +0200

    updating the project-1

---
+++ src/card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const card=()=> {
+  return (
+    <div className="card">
+        <img src='https://robohash.org/test?200*200' className="robo-logo" alt="ronots" />
+        <div>
+            <h2>Jane</h2>
+           <p> Jane@gmail.com</p>
+        </div>  
+    </div>
+  );
+}
+
+export default card;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import card from './card'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+  <card />
+  <card />
+  <card />
+  </div>
+  ,
   document.getElementById('root')
 );
 
