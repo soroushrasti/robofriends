d7efc940d0489122ed8385c9480703f9ad8653ec
Soroush Rasti <rasti.soroush@gmail.com>
2020-08-23 12:14:16 +0200

    adding App.js -7

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, {Component} from 'react';
 import CardList from './CardList';
 import {robots} from './robots';
 import SearchBox from './SearchBox'
+import './App.css'
 
 class App extends Component {
     constructor(){
         super();
-        this.state={robots:robots ,
+        this.state={robots:[] ,
         searchfield:' '}
+    }
+
+    componentDidMount(){
+        this.setState({robots:robots})
     }
 
    onsearchChange=(event)=>{
@@ -19,7 +24,7 @@ render(){
     const filteredrobots = this.state.robots.filter(robots => {return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase() ); });
 
     return (<div className='tc'>
-        <h1>RoboFriends</h1>
+        <h1 className='f1' >RoboFriends</h1>
         <SearchBox searchChange={this.onsearchChange} />
         <CardList robots={filteredrobots} />
         </div>);
