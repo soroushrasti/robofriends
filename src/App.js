55cbc1aa1ab42684ca3dc54722ade1e058545178
Soroush Rasti <rasti.soroush@gmail.com>
2020-08-23 11:17:22 +0200

    adding App.js -6

--- src/App.js
+++ src/App.js
@@ -9,17 +9,20 @@ class App extends Component {
         this.state={robots:robots ,
         searchfield:' '}
     }
-   render(){return (<div className='tc'>
-       <h1>RoboFriends</h1>
-       <SearchBox searchChange={this.onsearchChange} />
-       <CardList robots={this.state.robots} />
-       </div>);
-   }
+
    onsearchChange=(event)=>{
-      this.setState({searchfield:event.target.value});
-     const filteredrobots = this.state.robots.filter(robots => {return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase() ); })
+      this.setState({searchfield:event.target.value})
+
    }
+
+render(){
+    const filteredrobots = this.state.robots.filter(robots => {return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase() ); });
+
+    return (<div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onsearchChange} />
+        <CardList robots={filteredrobots} />
+        </div>);
 }
-
-
+}
 export default App;
