e900f2a1404856932628586478493f9bc57f706b
Soroush Rasti <rasti.soroush@gmail.com>
2020-08-24 15:43:57 +0200

    added scroll-1

--- src/Scroll.js
+++ src/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Scroll=(props)=>{
 return (
-    <div state={{overflowY:'scroll', border:'1px solid black' ,height: '800px' }}>
+    <div style={{overflowY:'scroll', border:'1px solid black' ,height: '800px' }}>
         {props.children}
     </div>
 )
